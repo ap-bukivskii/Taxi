abdda80db32e321a77f9f632296b544bafc07ca4
tolik <ap.bukivskii@gmail.com>
2019-06-02 21:11:34 +0300

    Small but necessary refactoring

--- src/main/java/controller/MainServlet.java
+++ src/main/java/controller/MainServlet.java
@@ -1,8 +1,7 @@
 package controller;
 
 import controller.command.Command;
-import controller.command.impl.common.*;
-import controller.command.impl.user.*;
+import controller.command.CommandFactory;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
@@ -11,52 +10,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Controller functional. Receives and proceeds http requests from Tomcat
  */
-
 public class MainServlet extends HttpServlet {
-    private Map<String, Command> commands = new HashMap<>();
     private static final Logger LOGGER = LogManager.getLogger(MainServlet.class);
 
 
     @Override
     public void init() throws ServletException { //TODO command factory
         LOGGER.info("Initializing main servlet");
-        //common commands
-        commands.put("login_do", new LoginCommand());
-        commands.put("logout", new LogoutCommand());
-        commands.put("home", new RedirectHome());
-        commands.put("lang_ua", new ChangeLangUa());
-        commands.put("lang_en", new ChangeLandEn());
-        commands.put("index", new RedirectIndex());
-        commands.put("register_do", new RegisterCommand());
-        commands.put("viewSpecials", new ViewSpecials());
-
-
-        //user commands
-        commands.put("findRide", new FindRideCommand());
-        commands.put("cancellRide", new CancelRideCommand());
-        commands.put("confirmRide", new ConfirmRideCommand());
-        commands.put("rideHistory", new RideHistoryCommand());
-
-
-
-        //admin commands
-
-
     }
 
     private void processUser(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         LOGGER.info("PROCESS USER METHOD");
         String path = req.getRequestURI();
         path = path.replaceAll(".*/action/", "");
-        Command command = commands.getOrDefault(path,new RedirectHome());
+        Command command = CommandFactory.getInstance().getCommand(path);
         String page = command.execute(req, resp);
-
         LOGGER.debug("responce page="+page);
         if (page.contains("redirect:")) {
             resp.sendRedirect(req.getContextPath() + page.replace("redirect:", ""));
@@ -75,5 +47,4 @@ public class MainServlet extends HttpServlet {
         LOGGER.debug("DO POST METHOD");
         processUser(req, resp);
     }
-
 }
---
+++ src/main/java/controller/command/CommandFactory.java
@@ -0,0 +1,51 @@
+package controller.command;
+
+import controller.command.impl.common.*;
+import controller.command.impl.user.*;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandFactory {
+    private static final Logger LOGGER = LogManager.getLogger(CommandFactory.class);
+    private static volatile CommandFactory instance;
+    private Map<String, Command> commands = new HashMap<>();
+
+    private CommandFactory() {
+        //common commands
+        commands.put("login_do", new LoginCommand());
+        commands.put("logout", new LogoutCommand());
+        commands.put("home", new RedirectHome());
+        commands.put("lang_ua", new ChangeLangUa());
+        commands.put("lang_en", new ChangeLandEn());
+        commands.put("index", new RedirectIndex());
+        commands.put("register_do", new RegisterCommand());
+        commands.put("viewSpecials", new ViewSpecials());
+
+        //user commands
+        commands.put("findRide", new FindRideCommand());
+        commands.put("cancellRide", new CancelRideCommand());
+        commands.put("confirmRide", new ConfirmRideCommand());
+        commands.put("rideHistory", new RideHistoryCommand());
+
+        //admin commands
+
+    }
+
+    public static CommandFactory getInstance(){
+        if(instance == null){
+            synchronized (CommandFactory.class){
+                if(instance == null){
+                    instance = new CommandFactory();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Command getCommand(String commandName){
+        return commands.getOrDefault(commandName,new RedirectHome());
+    }
+}
